d882443b28f5dcf1954306eb3c740ae8eac4dfe6
vincent <vincbuchholz@gmail.com>
2020-10-09 10:15:03 +0200

    -m GT

--- src/com/company/GameTest.java
+++ src/com/company/GameTest.java
@@ -1,4 +1,71 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class GameTest {
+    package com.company;
+import com.company.Account;
+import com.company.Player;
+import java.util.Scanner;
+    public class GameTest {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("press enter to continue:");
+            String enter = input.nextLine();
+
+            if (enter.contentEquals("")){
+                System.out.println("hej");
+            }
+
+
+
+            Account ac1 = new Account();
+            System.out.println(ac1);
+
+            ac1.AddToAccount(500);
+            System.out.println(ac1);
+
+            if (ac1.getBalance() == 1500) {
+                System.out.println("hej");
+            }
+
+            ac1.SubbFromAccount(1700);
+            System.out.println(ac1);
+
+
+
+        /*
+        Die diePlayerOne = new Die();
+        System.out.println(diePlayerOne);
+        diePlayerOne.RollDice();
+        System.out.println(diePlayerOne);
+        System.out.println(diePlayerOne.Sum());
+
+        if (diePlayerOne.Sum() > 6) {
+            System.out.println("hej");
+        }
+         */
+
+
+
+
+
+
+
+
+
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.print("player one enter your name: ");
+        Player playerOne = new Player(input.next());
+
+        System.out.println(playerOne);
+        */
+
+        }
+
+    }
+
 }
