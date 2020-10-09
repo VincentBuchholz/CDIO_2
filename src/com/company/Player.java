3a48874b9cf776c37b58422406f6317bc4d22950
vincent <vincbuchholz@gmail.com>
2020-10-09 10:13:53 +0200

    -m p1

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Player {
+    String playerName;
+
+    public Player(String name) {
+        playerName = name;
+    }
+
+    public String toString() {
+        String name = this.playerName;
+        return name;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
