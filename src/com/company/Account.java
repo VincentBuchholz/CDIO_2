11e60baa0ed922353f7a27dca9955866ef13866f
vincent <vincbuchholz@gmail.com>
2020-10-09 10:10:43 +0200

    -m a1

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,8 +1,35 @@
 package com.company;
 
 public class Account {
+    int balance = 1000;
+
+
+    public Account() {
+        this.balance = this.balance;
+
+    }
+
+    public void AddToAccount(int points) {
+        this.balance = this.balance + points;
+    }
+
+    public void SubbFromAccount(int points) {
+        this.balance = this.balance - points;
+        if (this.balance < 0) {
+            this.balance = 0;
+        }
+    }
+
+    public String toString() {
+        String points = "" + balance;
+        return points;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 }
