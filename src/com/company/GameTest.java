8ceec9e4c30ea656bf3a78411378533bff455975
Tobias <tubbe1088@gmail.com>
2020-10-28 13:58:53 +0100

    Rettelse af fejl

--- src/com/company/GameTest.java
+++ src/com/company/GameTest.java
@@ -1,9 +1,7 @@
 package com.company;
 
-import java.util.Scanner;
 
-public class GameTest {
-    package com.company;
+
 import com.company.Account;
 import com.company.Player;
 import java.util.Scanner;
@@ -68,4 +66,4 @@ import java.util.Scanner;
 
     }
 
-}
+
